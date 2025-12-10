e55a2aeb65a0e255c929ffa0d55e132eeec48d99
Vedad003 <ograsevicvedad2003@gmail.com>
2025-12-10 23:01:13 +0100

    dodane nove funkcionalnosti

--- src/main/java/financeapp/TransactionManager.java
+++ src/main/java/financeapp/TransactionManager.java
@@ -3,9 +3,13 @@ package financeapp;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class TransactionManager {
 
@@ -20,30 +24,40 @@ public class TransactionManager {
         collection.insertOne(t.toDocument());
     }
 
+    public void updateTransaction(Transaction t) {
+        collection.replaceOne(eq("_id", t.getId()), t.toDocument());
+    }
+
+    public void deleteTransaction(ObjectId id) {
+        collection.deleteOne(eq("_id", id));
+    }
+
     public ArrayList<Transaction> getAllTransactions() {
         ArrayList<Transaction> list = new ArrayList<>();
-        MongoCursor<Document> cursor = collection.find().iterator();
 
+        MongoCursor<Document> cursor = collection.find().iterator();
         while (cursor.hasNext()) {
+
             Document d = cursor.next();
 
-            // Sigurno čitanje polja amount
+            ObjectId id = d.getObjectId("_id");
+
             Object amountObj = d.get("amount");
             double amount = 0;
+
             if (amountObj instanceof Number) {
                 amount = ((Number) amountObj).doubleValue();
             }
 
-            // Sigurno čitanje tipa i opisa
             String type = d.getString("type") != null ? d.getString("type") : "Nepoznato";
-            String description = d.getString("description") != null ? d.getString("description") : "";
+            String desc = d.getString("description") != null ? d.getString("description") : "";
+            String category = d.getString("category") != null ? d.getString("category") : "ostalo";
 
-            list.add(new Transaction(type, amount, description));
+            list.add(new Transaction(id, type, amount, desc, category));
         }
 
         return list;
     }
-
 
     public double getTotalIncome() {
         double total = 0;
@@ -64,4 +78,17 @@ public class TransactionManager {
         }
         return total;
     }
+
+    public double getCategoryTotal(String category) {
+        double total = 0;
+
+        for (Transaction t : getAllTransactions()) {
+            if (t.getType().equals("Rashod") && t.getCategory().equalsIgnoreCase(category)) {
+                total += t.getAmount();
+            }
+        }
+
+        return total;
+    }
+
 }
